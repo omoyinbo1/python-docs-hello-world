a6e8a8d3e42057a29a43bd5bd49a073567abacd8
ftfly <ftfly@126.com>
2019-09-04 14:52:56 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,3 @@
-import json
-import logging
-import azure.functions as func
-from azure.keyvault import KeyVaultClient
-from azure.storage.blob import BlockBlobService
-from msrestazure.azure_active_directory import MSIAuthentication
 from flask import Flask
 app = Flask(__name__)
 
