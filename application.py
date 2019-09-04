0b8c5f47bface68369a3664b47311ea18dc05073
ftfly <ftfly@126.com>
2019-09-04 15:15:24 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from azure.keyvault import KeyVaultClient
 from azure.storage.blob import BlockBlobService
+from msrestazure.azure_active_directory import MSIAuthentication
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Python!"
+    credentials = MSIAuthentication(resource='https://vault.azure.net')
+    kvclient = KeyVaultClient(credentials)
+    key = kvclient.get_secret("https://mnanalyticssandbox-vault.vault.azure.net/", "DiskEncrption", "").value
+    return key
